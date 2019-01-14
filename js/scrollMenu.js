8f5d41b1e78e7114b6c341aa3f479113bb892cd0
Łukasz Wcisło <lukasz.wcislo87@gmail.com>
2019-01-14 23:23:55 +0100

    changed logo, used pseudoelement after in section contact

--- js/scrollMenu.js
+++ js/scrollMenu.js
@@ -1,13 +1,14 @@
- 
-$(window).scroll(function(){
+ $(window).scroll(function(){
    var top = $(window).scrollTop();
    var find_class_small = $.contains('nav', '.black');
  
    if(top > 50 && find_class_small == false) {
     $('nav').addClass('black');  
+    $('.logo').addClass('black');  
    }
    else {
     $('nav').removeClass('black'); 
+    $('.logo').removeClass('black'); 
    }
  
 });
